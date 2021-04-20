d9c50e23acbc8a9f1a924c388cca118b68e5de28
Tasja <tasjalaurie@outlook.com>
2021-04-20 17:55:57 +1000

    update import/exports

--- src/utils/postcodes.js
+++ src/utils/postcodes.js
@@ -1,12 +1,12 @@
-const axios = require('axios');
+import axios from 'axios';
 
 const BASE_URL = 'https://api.postcodes.io/postcodes';
 
-module.exports = {
-  getPostcodeData: (postcodes) =>
-    axios
-      .post(`${BASE_URL}`, {
-        postcodes: postcodes
-      })
-      .then((res) => res.data)
-};
+const getPostcodeData = (postcodes) =>
+  axios
+    .post(`${BASE_URL}`, {
+      postcodes: postcodes
+    })
+    .then((res) => res.data);
+
+export { getPostcodeData };
